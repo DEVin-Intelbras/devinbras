df9d7386c06e15315fb0941c82f4fc88effa9902
Michael Nascimento <michael.nsc@outlook.com>
2022-08-25 14:54:07 -0300

    refactor: adiciona tabela ao NewProduct

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -1,4 +1,4 @@
-import { ButtonOutline, Formulario } from "@components";
+import { ButtonOutline, Formulario, Tabela } from "@components";
 import styles from "./NewProduct.module.css";
 
 export const NewProduct = () => {
@@ -9,40 +9,48 @@ export const NewProduct = () => {
           <Formulario.Titulo>Novo Produto</Formulario.Titulo>
           <Formulario.Conteudo>
             <Formulario.Input
+              erro=""
               label="Nome"
               placeholder="Informe o nome"
               tamanho={8}
             />
             <Formulario.Input
+              erro=""
               label="Valor"
               placeholder="Informe o valor"
               tamanho={4}
             />
             <Formulario.Input
+              erro=""
               label="Url Imagem 1"
               placeholder="Informe a url da imagem"
               tamanho={4}
             />
             <Formulario.Input
+              erro=""
               label="Url Imagem 2"
               placeholder="Informe a url da imagem"
               tamanho={4}
             />
             <Formulario.Input
+              erro=""
               label="Url Imagem 3"
               placeholder="Informe a url da imagem"
               tamanho={4}
             />
             <Formulario.TextArea
+              erro=""
               label="Url Imagem 3"
               placeholder="Informe a url da imagem"
             />
             <Formulario.Input
+              erro=""
               label="Url Imagem 3"
               placeholder="Informe a url da imagem"
               tamanho={5}
             />
             <Formulario.Input
+              erro=""
               label="Url Imagem 3"
               placeholder="Informe a url da imagem"
               tamanho={5}
@@ -50,7 +58,26 @@ export const NewProduct = () => {
             <Formulario.Componente tamanho={2}>
               <ButtonOutline fullWidth>Adicionar</ButtonOutline>
             </Formulario.Componente>
+            <Formulario.Componente tamanho={12}>
+              <Tabela.Conteudo
+                onApagarLinha={(i) => {
+                  console.log(i);
+                }}
+              >
+                <Tabela.Linha>
+                  <Tabela.Celula destaque>Frequencia de operação</Tabela.Celula>
+                  <Tabela.Celula>50/60hz</Tabela.Celula>
+                </Tabela.Linha>
+                <Tabela.Linha>
+                  <Tabela.Celula destaque>Altura do produto</Tabela.Celula>
+                  <Tabela.Celula>12 cm</Tabela.Celula>
+                </Tabela.Linha>
+              </Tabela.Conteudo>
+            </Formulario.Componente>
           </Formulario.Conteudo>
+          <Formulario.Componente tamanho={12}>
+            <ButtonOutline>Salvar</ButtonOutline>
+          </Formulario.Componente>
         </Formulario.Container>
       </div>
     </div>
